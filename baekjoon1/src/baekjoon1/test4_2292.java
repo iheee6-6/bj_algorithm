e01dc3264c7d9b6489de1dc5f042038c90ac458f
iheee6-6 <iheee6@gmail.com>
2021-05-09 19:06:12 +0900

    2292

---
+++ baekjoon1/src/baekjoon1/test4_2292.java
@@ -0,0 +1,28 @@
+package baekjoon1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test4_2292 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num=0;
+		try {
+			 num =Integer.parseInt(br.readLine());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int n=0;
+		while(true) {
+			if((n+1)>=num) {
+				if(n%2==0) 
+			}
+			
+		}
+		
+	}
+
+}
