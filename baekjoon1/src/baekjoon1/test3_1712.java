45fc5323020eb8b5783496c312d94c15960f2869
iheee6-6 <iheee6@gmail.com>
2021-05-09 17:37:43 +0900

    0509 1712 class

--- baekjoon1/src/baekjoon1/test3_1712.java
+++ baekjoon1/src/baekjoon1/test3_1712.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class test3_1712 {
-	public int solution() {
+	public void solution() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String inputNum = "";
 		try {
@@ -18,13 +18,7 @@ public class test3_1712 {
 		int B=Integer.parseInt(numArr[1]);
 		int C=Integer.parseInt(numArr[2]);
 
-		if(B>C) return -1;
-		int num=1;
-		while(true) {
-			if((A+B*num)-C*num<0) {
-				return num;
-			}
-			num++;
-		}
+		if(B>=C) System.out.println(-1);
+		else System.out.println((A/(C-B))+1);
 	}
 }
