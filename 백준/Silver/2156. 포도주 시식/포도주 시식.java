21105892f675b3332da944689d9a192b9d6d7fbb
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-01-03 22:23:33 +0900

    [Silver I] Title: 포도주 시식, Time: 144 ms, Memory: 14788 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,23 +1,33 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); 
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[10001];
-        for(int i = 1; i <= n; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        int[] wine = new int[n];
+        int[] dp = new int[n];
+
+        for(int i = 0; i < n; i++){
+            wine[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] dp = new int[10001];
-        dp[1] = arr[1];
-        dp[2] = arr[2] + arr[1];
+        dp[0] = wine[0]; 
 
-        for(int i = 3; i <= n; i++){
-            dp[i] = Math.max(dp[i-3] + arr[i-1] + arr[i], dp[i-2] + arr[i]);
-            dp[i] = Math.max(dp[i-1], dp[i]);
+        if(n > 1){
+            dp[1] = wine[0] + wine[1];
         }
-        System.out.println(dp[n]);
+        
+        if(n > 2){
+            dp[2] = Math.max(dp[1], Math.max(wine[0]+wine[2], wine[1] + wine[2]));
+        
+            for(int i = 3; i < n; i++){
+                dp[i] = Math.max(dp[i-2] + wine[i], Math.max(dp[i-1], dp[i-3] + wine[i-1] + wine[i]));
+            }
+        }
+        
+        System.out.println(dp[n-1]);
     }
 }
