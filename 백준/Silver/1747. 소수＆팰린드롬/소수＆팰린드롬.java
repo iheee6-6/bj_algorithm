6edbaffa903c23dbec8bf0f058b3d942bf5fe73a
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-01-06 00:58:20 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 236 ms, Memory: 39080 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -1,35 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
- 
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
- 
-        for (int i = N; i < 999999999; i++) {
-            if (isPrime(i) && isPalindrom(i)) {
-                System.out.println(i);
-                break;
-            }
-        }
-    }
- 
-    private static boolean isPalindrom(int num) {
-        StringBuilder sb = new StringBuilder(num + "");
-        if (!sb.toString().equals(sb.reverse().toString()))
-            return false;
-        return true;
-    }
- 
-    private static boolean isPrime(int num) {
-        if (num == 1)
-            return false;
- 
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0)
-                return false;
-        }
-        return true;
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		
+		for(int i=0;;i++) {
+			if(palind(num+i)&&prime(num+i)) {
+				System.out.println(num+i);
+				break;
+			}
+		}
+	}
+	static boolean prime(int num) {
+		if(num==0||num==1) return false;
+		else if(num==2) return true;
+		for(int i=2;i<num;i++) {
+			if(num%i==0) return false;
+		}
+		return true;
+	}
+	
+	static boolean palind(int num) {
+		String str = Integer.toString(num);
+		for(int i=0;i<str.length()/2;i++) {
+			if(str.charAt(i)==str.charAt(str.length()-i-1)) continue;
+			else return false;
+		}
+		return true;
+	}
 }
