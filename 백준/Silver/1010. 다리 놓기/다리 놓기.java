f5ccc5ce97623fc67bdaea0a5381e9c7f9dceb5b
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-01-06 00:02:43 +0900

    [Silver V] Title: 다리 놓기, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,50 +1,31 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	
 	static int[][] dp = new int[30][30];
- 
-	public static void main(String[] args) throws IOException {
-		
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st;
-		
+		int cnt = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < T; i++) {
-			
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			// M개중 N개를 뽑는 경우이므로 nCr 에서 n = M, r = N이다.
-			int N = Integer.parseInt(st.nextToken());	// N = r
-			int M = Integer.parseInt(st.nextToken());	// M = n
-					
-			
-			sb.append(combi(M, N)).append('\n');
+        StringTokenizer st;
+		for (int i = 0; i < cnt; i++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			sb.append(comb(m, n)).append('\n');
 		}
-		
 		System.out.println(sb);
- 
 	}
-	
-	static int combi(int n, int r) {
-		
-		// 이미 풀린 경우 바로 반환
-		if(dp[n][r] > 0) {
-			return dp[n][r]; 
-		}
-		
-		// 2번 성질
-		if(n == r || r == 0) {
-			return dp[n][r] = 1;
-		}
-		
-		// 1번 성질
-		return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
+
+	static int comb(int m, int n) {
+		if (dp[m][n] > 0)
+			return dp[m][n];
+		if (m == n || n == 0)
+			return dp[m][n] = 1;
+		return dp[m][n]=comb(m - 1, n - 1) + comb(m - 1, n);
+
 	}
 }
