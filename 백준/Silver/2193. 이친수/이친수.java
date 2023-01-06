482e80ce54da8c6d1d9df780409cb74c17afd066
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-01-06 20:40:15 +0900

    [Silver III] Title: 이친수, Time: 120 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,17 +1,19 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-    public static void main(String args[]) throws IOException{
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int count = Integer.parseInt(br.readLine());
-            long d[] = new long[count+1];
-            d[0] = 0;
-            d[1] = 1;
-            for (int i = 2; i <= count; i++){
-                d[i] = d[i-1] + d[i-2];
-            }
-            System.out.println(d[count]);
-    }
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+	    long[] dp = new long[num+1];
+		dp[1]=1;
+		dp[0]=0;
+		if(num>1) 
+			dp[2]=1;
+		for(int i=3;i<num+1;i++) {
+			dp[i]=dp[i-1]+dp[i-2];
+		}
+		System.out.println(dp[num]);
+	}
 }
