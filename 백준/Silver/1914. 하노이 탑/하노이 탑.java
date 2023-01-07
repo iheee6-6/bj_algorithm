f44bf3c1b8c28877caa70326faef596419e3e138
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-01-08 00:30:20 +0900

    [Silver I] Title: 하노이 탑, Time: 544 ms, Memory: 69216 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.java
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.java
@@ -1,33 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.Scanner;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
+	static StringBuilder sb = new StringBuilder();
 
-    public static void hanoi(int num, int from, int by, int to) {
-        if(num == 1) {
-            sb.append(from + " " + to + "\n");
-        } else {
-            hanoi(num-1, from, to, by); // 1번에서 2번으로
-            sb.append(from + " " + to + "\n");
-            hanoi(num-1, by, from, to); // 2번에서 3번으로
-        }
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BigInteger res = new BigInteger("1");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		BigInteger count = new BigInteger("2");
+		System.out.println(count.pow(num).subtract(new BigInteger("1")));
+		/*for (int i = 0; i < num; ++i) {
+			res = res.multiply(new BigInteger("2")); // 2의 N 제곱
+		}
+		res = res.subtract(new BigInteger("1")); // 빼기 1
+		System.out.println(res);
+		*/
+		if (num <= 20) {
+			//System.out.println((int) Math.pow(2, num) - 1);
+			Haoni(num, 1, 2, 3);
+			System.out.println(sb);
+		} 
+	}
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        BigInteger res = new BigInteger("1");
-        int N = scan.nextInt();
-        if (N<=20) { 
-            hanoi(N, 1, 2, 3);
-            sb.insert(0, (int)(Math.pow(2, N) - 1) + "\n");
-            System.out.print(sb);
-        } else { // 20 초과하면 BigInteger로 이동 횟수만 출력
-            for(int i=0; i<N; ++i) {
-                res = res.multiply(new BigInteger("2")); // 2의 N 제곱
-            }
-            res = res.subtract(new BigInteger("1")); // 빼기 1
-            System.out.println(res);
-        }
-    }
+	static void Haoni(int num, int start, int middle, int end) {
+		if (num == 1) {
+			sb.append(start + " " + end + "\n");
+		} else {
+			Haoni(num - 1, start, end, middle);
+			sb.append(start + " " + end + "\n");
+			Haoni(num - 1, middle, start, end);
+		}
+	}
 }
