21575798a4d7b76e8a1d421c416204dd3b8e6d0a
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-01-15 22:52:16 +0900

    [Silver I] Title: 정수 삼각형, Time: 284 ms, Memory: 27264 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -19,15 +19,12 @@ public class Main {
 				arr[i][j]=Integer.parseInt(st.nextToken());
 			}
 		}
-		for (int i = 0; i < num; i++) {
-			dp[num - 1][i] = arr[num - 1][i];
-		}
-		find(0,0);
-		System.out.println(dp[0][0]);
+		
+		System.out.println(find(0,0));
 	}
 
 	static int find(int depth,int side) {
-		if(depth==num-1) return dp[depth][side];
+		if(depth==num-1) return arr[depth][side];
 		if(dp[depth][side]==0) {
 			dp[depth][side]=Math.max(find(depth+1,side),find(depth+1,side+1))+arr[depth][side];
 		}
