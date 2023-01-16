2b9cbfe51c41ac8f15fc2c1f38f70ecc86637e12
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-01-16 18:45:51 +0900

    [Gold III] Title: 내리막 길, Time: 384 ms, Memory: 35768 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,49 +1,56 @@
-import java.io.*;
-import java.util.*;
-// dfs + dp 문제 
-public class Main{
-	static int N, M;
-	static int[][] map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] arr;
+	static boolean[][] check;
 	static int[][] dp;
-	static int[] dx = {-1,0,1,0};
-	static int[] dy = {0,-1,0,1};
-	public static void main(String[] args) throws Exception{
+	static long result = 0;
+	static int[] dx = { 0, 0, 1, -1 };
+	static int[] dy = { 1, -1, 0, 0 };
+	static int min, N, M;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine() , " ");
-		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
+		arr = new int[N][M];
 		dp = new int[N][M];
-		
-		for(int i = 0 ; i < N ; i ++) {
-			st = new StringTokenizer(br.readLine() , " ");
-			for(int j = 0 ; j < M ; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				// -1 로 초기화 , 방문할시 0처리 
-				dp[i][j] = -1;
+		check = new boolean[N][M];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				int x = Integer.parseInt(st.nextToken());
+				arr[i][j] = x;
+				dp[i][j]=-1;
 			}
 		}
-		
-		System.out.println(searchDown(0,0));
+
+		System.out.println(dfs(N - 1, M - 1));
+
 	}
-	private static int searchDown(int x, int y) {
-		if(x == N-1 && y == M-1) {
+
+	static int dfs(int x, int y) {
+		if (x == 0 && y == 0)
 			return 1;
-		}
-		// dfs 방식이므로 dp가 -1 아니라면 이미 탐색한 경로이므로 return dp;
-		if(dp[x][y] != -1) {
-			return dp[x][y];
-		}
-		
-		dp[x][y] = 0;
-		for(int i = 0 ; i < 4 ; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if(0 > nx || nx >= N || 0>ny || ny>= M || map[x][y] <= map[nx][ny]) continue;
-			// 가능한 경로를 dp += 1
-			dp[x][y] += searchDown(nx, ny);
+		if (dp[x][y] == -1) {
+			dp[x][y]=0;
+			for (int i = 0; i < 4; i++) {
+				min = arr[x][y];
+				int xx = x + dx[i];
+				int yy = y + dy[i];
+				if (xx < N && yy < M & xx >= 0 && yy >= 0) {
+					if (min < arr[xx][yy]) {
+						dp[x][y] += dfs(xx, yy);
+					}
+				}
+			}
 		}
 		return dp[x][y];
 	}
+
 }
