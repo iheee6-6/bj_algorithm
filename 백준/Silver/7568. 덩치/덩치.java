41d642da06baadf7eddf61ce0f7a54335863e12a
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-01-19 14:13:22 +0900

    [Silver V] Title: 덩치, Time: 140 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,36 +1,34 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
- 
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
- 
-		int[][] arr = new int[N][2];
- 
-		String[] sp;
-		for(int i = 0; i < N; i++) {
-			sp = br.readLine().split(" ");			// 문자열 분리
-			arr[i][0] = Integer.parseInt(sp[0]);	// [i][0] : 몸무게 
-			arr[i][1] = Integer.parseInt(sp[1]);	// [i][1] : 키 
+		int per = Integer.parseInt(br.readLine());
+		int[][] arr = new int[per][2];
+		StringTokenizer st;
+		for (int i = 0; i < per; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-				
-		for(int i = 0; i < N; i++) {
-			int rank = 1;
-			
-			for(int j = 0; j < N; j++) {
-				if(i == j) continue;
-				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
-					rank++;
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<per;i++) {
+			int count=1;
+			for(int j=0;j<per;j++) {
+				if(i==j) continue;
+				if(arr[i][0]<arr[j][0] &&arr[i][1]<arr[j][1]) {
+					count++;
 				}
 			}
- 
-			System.out.print(rank + " ");
+			sb.append(count+" ");
+			
 		}
- 
+		System.out.println(sb);
 	}
+
 }
