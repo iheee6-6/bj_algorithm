70c209e7d8f02e1c0666c0cec862d4ab70e3756c
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-02-08 21:35:58 +0900

    [Gold V] Title: 동전 2, Time: 140 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,29 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int num = Integer.parseInt(st.nextToken());
+		int k=Integer.parseInt(st.nextToken());
+		int[] arr = new int[num];
+		int[] dp = new int[k+1];
+		for(int i=0;i<num;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		Arrays.fill(dp, 100001);
+		dp[0]=0;
+		for(int i=0;i<num;i++) {
+			for(int j=arr[i];j<=k;j++) {
+				dp[j]=Math.min(dp[j],dp[j-arr[i]]+1);
+			}
+		}
+		System.out.println(dp[k]==100001?-1:dp[k]);
+		
+	}
 
-        int[] coin = new int[n];
-        for (int i = 0; i < n; i++) {
-            coin[i] = sc.nextInt();
-        }
-
-        int[] dp = new int[k + 1];
-        Arrays.fill(dp, 100001);
-        dp[0] = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = coin[i]; j <= k; j++) {
-                dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
-            }
-        }
-
-        System.out.println(dp[k] == 100001 ? -1 : dp[k]);
-        sc.close();
-    }
 }
