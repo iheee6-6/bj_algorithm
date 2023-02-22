8fc4414fadea8df401f386ec73dcdf9e9bc785d2
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-02-22 17:00:11 +0900

    [Gold II] Title: 가운데를 말해요, Time: 492 ms, Memory: 33440 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,33 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> qp = new PriorityQueue<>();
+		PriorityQueue<Integer> maxqp = new PriorityQueue<>(Collections.reverseOrder());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			if (qp.size() == maxqp.size()) {
+				maxqp.add(Integer.parseInt(br.readLine()));
+			} else {
+				qp.add(Integer.parseInt(br.readLine()));
+			}
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((o1, o2) -> o1 - o2);
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+			if (!qp.isEmpty()&&maxqp.peek() > qp.peek()) {
+				qp.add(maxqp.poll());
+				maxqp.add(qp.poll());
+			}
+			sb.append(maxqp.peek()).append("\n");
+		}System.out.println(sb);
 
-        for(int i = 0 ; i < n; i++){
-            int num = Integer.parseInt(br.readLine());
+	}
 
-            if(minHeap.size() == maxHeap.size()) maxHeap.offer(num);
-            else minHeap.offer(num);
-
-            if(!minHeap.isEmpty() && !maxHeap.isEmpty())
-                if(minHeap.peek() < maxHeap.peek()){
-                    int tmp = minHeap.poll();
-                    minHeap.offer(maxHeap.poll());
-                    maxHeap.offer(tmp);
-                }
-
-            sb.append(maxHeap.peek() + "\n");
-        }
-        System.out.println(sb);
-    }
 }
