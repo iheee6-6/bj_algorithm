698c18c165ed0d67a338ff7e444b3811bd4d717a
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-03-05 02:10:16 +0900

    [Silver II] Title: 나무 자르기, Time: 532 ms, Memory: 119340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,42 +1,76 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int trees[] = new int[N];
-	
-		// 나무길이 입력받기 + right 값은 나무 최대 길이 찾아 저장
-		int left = 0, right = Integer.MIN_VALUE;
-		st = new StringTokenizer(in.readLine(), " ");
+		
+		int[] tree = new int[N];
+		
+		int min = 0;
+		int max = 0;
+		
+		st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			trees[i] = num;
-			right = Math.max(right, num);
-		}
-	
-		while(left <= right) {
-			int mid = (left + right) / 2;
-			long sum = 0;
+			tree[i] = Integer.parseInt(st.nextToken());
 			
-			// 잘라진 나무 길이의 합
-			for(int i = 0; i < N; i++) {
-				// 이 조건을 넣지 않으면 음수값이 더해지기 때문에 제대로 된 계산이 되지 않음
-				if(trees[i] > mid) sum += trees[i] - mid; 
+			/*
+			 * 나무들 중 최댓값을 구하기 위해 매 입력 때마다 max 변수와 비교하여
+			 * 입력 받은 나무가 max보다 클 경우 max 값을 해당 나무의 높이로 갱신해준다. 
+			 */
+			if(max < tree[i]) {
+				max = tree[i];
 			}
-			
-			if(M <= sum) left = mid + 1;
-			else right = mid - 1;
 		}
 		
-		System.out.println(right);		
-		in.close();
-
+		// 이분 탐색 (upper bound)
+		while(min < max) {
+			
+			int mid = (min + max) / 2;
+			long sum = 0;
+			for(int treeHeight : tree) {
+				
+				/*
+				 *  tree의 잘린 길이 = tree의 높이 - 자르는 위치(mid)
+				 *  tree의 잘린 길의의 합을 구한다.
+				 *  이 때 자르는 위치가 주어진 나무의 높이보다 클 수 있기 때문에
+				 *  0 이하인 경우(=음수)에는 합산을 하지 않고 양수만 합산하도록 해야한다.
+				 */
+				if(treeHeight - mid > 0) { 
+					sum += (treeHeight - mid);
+				}
+			}
+			
+ 
+			/*
+			 * 자른 나무 길의의 합이 M보다 작다는 것은
+			 * 자르는 위치(높이)가 높다는 의미이므로 높이를 낮춰야 한다.
+			 * 즉, 상한(max)를 줄여야 한다.
+			 */
+			if(sum < M) {
+				max = mid;
+			}
+			
+			/*
+			 * 자른 나무 길이의 합이 M보다 크다는 것은
+			 * 자르는 위치(높이)가 낮다는 의미이므로 높이를 높여야 한다.
+			 * 또한, 같을 경우에는 최대한 적게 자르기 위해 자르는 높이를
+			 * 높여야 하므로 하한(min)을 올려야 한다.
+			 */
+			else {
+				min = mid + 1;
+			}
+		}
+		
+		System.out.println(min - 1);
+		
 	}
 }
