cb7b4baeffe6d4415de60234f5276ac29583b858
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-03-24 18:32:43 +0900

    [Gold III] Title: 환상의 짝꿍, Time: 1004 ms, Memory: 20860 KB -BaekjoonHub

--- 백준/Gold/15711. 환상의 짝꿍/환상의 짝꿍.java
+++ 백준/Gold/15711. 환상의 짝꿍/환상의 짝꿍.java
@@ -1,65 +1,64 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static boolean[] isPrime = new boolean[2_000_001];
+	public static boolean[] isPrime = new boolean[2000001];
     public static List<Integer> list = new ArrayList<>();
+    
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		eratosthenes();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			long a = Long.parseLong(st.nextToken());
+			long b = Long.parseLong(st.nextToken());
+			String result = "NO";
+			long sum = a + b;
+			if (sum < 4) {
+				sb.append("NO").append("\n");
+			} else if (sum % 2 == 0) {
+				sb.append("YES").append("\n");
+			} else {
+				if (check(sum - 2)) {
+					sb.append("NO").append("\n");
+				} else {
+					sb.append("YES").append("\n");
+				}
+			}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+		}
+		System.out.println(sb);
+	}
 
-        int T = Integer.parseInt(br.readLine());
-        eratosthenes();
-
-        while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            long A = Long.parseLong(st.nextToken());
-            long B = Long.parseLong(st.nextToken());
-
-            long sum = A + B;
-
-            if (sum < 4) {
-                sb.append("NO").append("\n");
-            } else if (sum % 2 == 0) {
-                sb.append("YES").append("\n");
-            } else {
-                if (check(sum - 2)) {
-                    sb.append("YES").append("\n");
-                } else {
-                    sb.append("NO").append("\n");
-                }
-            }
-        }
-
-        System.out.println(sb);
-    }
-
-    public static boolean check(long x) {
-        if (x <= 2_000_000) return !isPrime[(int) x];
-
-        for (int i = 0; i < list.size(); i++) {
-            if (x % list.get(i) == 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static void eratosthenes() {
-        isPrime[0] = isPrime[1] = true;
-
-        for (int i = 2; i <= 2_000_000; i++) {
-            if (!isPrime[i]) {
-                list.add(i);
-                for (int j = i * 2; j <= 2_000_000; j += i)
-                    isPrime[j] = true;
-            }
-        }
-    }
+	static boolean check(long num) {
+		if(num<=2000000) return isPrime[(int)num];
+		
+		for(int i=0;i<list.size();i++) {
+			if(num%list.get(i)==0) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	static void eratosthenes() {
+		isPrime[0]= isPrime[1]=true;
+		for(int i=2;i<=2000000;i++) {
+			if(!isPrime[i]) {
+				list.add(i);
+				for(int j=i*2;j<=2000000;j+=i) {
+					isPrime[j]=true;
+				}
+			}
+		}
+	}
 }
