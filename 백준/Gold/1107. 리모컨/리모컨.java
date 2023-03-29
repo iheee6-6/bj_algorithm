8a5dfaef488622c1975c3d93c1dae2c84276d23e
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-03-29 10:42:36 +0900

    [Gold V] Title: 리모컨, Time: 412 ms, Memory: 66592 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,36 +1,49 @@
-import java.util.*;
- 
-public class Main {    
-    
-    public static void main(String[] args)  {
-        Scanner scan = new Scanner(System.in);
-        
-        int target = scan.nextInt();
-        int m = scan.nextInt();
-        
-        boolean[] broken = new boolean[10];
-        for(int i = 0; i < m; i++) {
-            int n = scan.nextInt();
-            broken[n] = true;
-        }
-        
-        int result = Math.abs(target - 100); //초기값 설정
-        for(int i = 0; i <= 999999; i++) {
-            String str = String.valueOf(i);
-            int len = str.length();
-            
-            boolean isBreak = false;
-            for(int j = 0; j < len; j++) {
-                if(broken[str.charAt(j) - '0']) { //고장난 버튼을 눌러야 하면
-                    isBreak = true; 
-                    break; //더 이상 탐색하지 않고 빠져나온다.
-                }
-            }
-            if(!isBreak) { //i를 누를때 고장난 버튼을 누르지 않는다면
-                int min = Math.abs(target - i) + len; //i를 누른 후(len) target까지 이동하는 횟수(target - i)
-                result = Math.min(min, result);
-            }
-        }        
-        System.out.println(result);
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int now = 100;
+	static int result = Integer.MAX_VALUE;
+	static PriorityQueue<Character> pq;
+	static int n;
+	static int m;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		pq = new PriorityQueue<>();
+		if (m > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < m; i++) {
+				pq.add(st.nextToken().charAt(0));
+			}
+		}
+
+		int d = n - now;
+		int count=0;
+		for(int i=0;i<999999;i++) {
+			String str = String.valueOf(i);
+			boolean check=false;
+			for(int j=0;j<str.length();j++) {
+				if(pq.contains(str.charAt(j))) {
+					check=true; break;
+				}
+			}
+			
+			if(check) continue;
+			count=str.length()+Math.abs(n-i);
+			result= Math.min(result, count);
+			
+		}
+
+		System.out.println(Math.min(Math.abs(d), result));
+
+	}
+
 }
