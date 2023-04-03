ab63848a87e3527071c41ffdfca4a04813ef82f8
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-04-04 00:29:26 +0900

    [Gold V] Title: AC, Time: 896 ms, Memory: 101444 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -3,15 +3,13 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in) );
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		
 		while(T-- > 0) {
@@ -46,15 +44,14 @@ public class Main {
 			if(errYN) {
 				bw.write("error\n");
 			} else {
-				String str = "";
-				
-				if(dir) str = deq.toString().replaceAll(" ", "");
-				else {
-					Collections.reverse((List<?>)deq);
-					str = deq.toString().replaceAll(" ", "");
+				sb = new StringBuilder();
+				bw.write("[");
+				while(!deq.isEmpty()) {
+					if(dir) sb.append(deq.pollFirst());
+					else sb.append(deq.pollLast());
+					if(!deq.isEmpty()) sb.append(",");
 				}
-				
-				bw.write(str + "\n");
+				bw.write(sb + "]\n");
 			}
 		}
 		
