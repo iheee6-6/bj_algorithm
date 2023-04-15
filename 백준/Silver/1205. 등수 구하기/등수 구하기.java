2ef73fce59aefe11dd744e80c9e0c737c33f87e1
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-04-16 00:29:35 +0900

    [Silver IV] Title: 등수 구하기, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -1,29 +1,34 @@
-import java.util.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
-    public static void main (String[]args){
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int new_score = scanner.nextInt();
-        int p = scanner.nextInt();  
 
-        Integer[] arr = new Integer[n];
-        for(int i=0; i<n; i++){
-            arr[i] = scanner.nextInt();
-        }
-        Arrays.sort(arr, Collections.reverseOrder());
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int score = scanner.nextInt();
+		int p = scanner.nextInt();
 
-        if(n == p && new_score <= arr[arr.length-1])
-            System.out.print(-1);
-        else{
-            int rank = 1;
-            for(int i=0; i<arr.length; i++){
-                if(new_score < arr[i])
-                    rank++;
-                else
-                    break;
-            }
-            System.out.print(rank);
-        }
-    }
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			list.add(scanner.nextInt());
+		}
+		Collections.sort(list, Collections.reverseOrder());
+
+		int result = 1;
+		if (n == p && score <= list.get(n - 1))
+			result = -1;
+		else {
+			for (int i = 0; i < n; i++) {
+				if (list.get(i) > score)
+					result++;
+				else
+					break;
+			}
+		}
+		System.out.println(result);
+	}
+
 }
