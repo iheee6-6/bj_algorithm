ba4025a7470bbc361803a849a03caaa0e4a22e99
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-05-06 11:45:08 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 168 ms, Memory: 15136 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -3,46 +3,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
-public class Main{
-	public static int[] arr;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));	
-        int n = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st =new StringTokenizer(br.readLine());
-        arr =  new int[n];
-        for(int i = 0; i < n; i++) {
-        	arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        int[] dp = new int[n];
-        dp[0] = 1;
-        int lis = 1;
-        for(int i = 1; i < n; i++) {
-        	dp[i] = 1;
-        	for(int j = 0; j < i; j++) {
-        		if(arr[j] < arr[i]) {
-        			//arr : 1 5 6 2 3 4
-        			//dp : 1 2 3 2 3 4
-        			dp[i] = Math.max(dp[i], dp[j] + 1);
-        			lis = Math.max(lis, dp[i]);
-        		}
-        	}
-        }
-        StringBuilder sb = new StringBuilder();
-        sb.append(lis + "\n");
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i = n-1; i >= 0; i--) {
-        	if(dp[i] == lis) {
-        		stack.push(arr[i]);
-        		lis--;
-        	}
-        }
-        
-        while(!stack.isEmpty()) {
-        	sb.append(stack.pop() + " ");
-        }
-        System.out.println(sb.toString());
-    }
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[num];
+		for (int i = 0; i < num; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] dp = new int[num];
+		dp[0]=1;
+		int max=1;
+		for(int i=1;i<num;i++) {
+			dp[i]=1;
+			for(int j=0;j<i;j++) {
+				if(arr[i]>arr[j]) {
+					dp[i]=Math.max(dp[i], dp[j]+1);
+					max=Math.max(max, dp[i]);
+				}
+			}
+		}
+		
+		System.out.println(max);
+		Stack<Integer> s = new Stack<>();
+		for(int i=num-1;i>=0;i--) {
+			if(dp[i]==max) {
+				s.add(arr[i]);
+				max--;
+			}
+		}
+		
+		while(!s.isEmpty()) {
+			System.out.print(s.pop()+" ");
+		}
+
+	}
+
 }
