6eba8e21c59832209e30abc1a0cf8aaa03132152
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-05-23 12:40:10 +0900

    [Silver III] Title: 계단 오르기, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,36 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-	static Integer[] dp;
-	static int[] score;
+
+	static int[] dp;
+	static int[] arr;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int cnt = Integer.parseInt(br.readLine());
-		dp = new Integer[cnt + 1];
-		score = new int[cnt + 1];
-		for (int i = 1; i < cnt + 1; i++) {
-			score[i] = Integer.parseInt(br.readLine());
+
+		arr = new int[cnt + 1];
+		for (int i = 1; i <= cnt; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 
-		dp[0] = score[0];	// 디폴트값이 null이므로 0으로 초기화 해주어야한다.
-		dp[1] = score[1];
-		
-		if(cnt >= 2) {
-			dp[2] = score[1] + score[2];
+		dp = new int[cnt + 1];
+		Arrays.fill(dp, -1);
+		dp[0] = 0;
+		dp[1] = arr[1];
+		if (cnt >= 2) {
+			dp[2] = arr[1] + arr[2];
 		}
-		
-		System.out.println(find(cnt));
+
+		find(cnt);
+		System.out.println(dp[cnt]);
 	}
 
-	static int find(int N) {
-		// 아직 탐색하지 않는 N번째 계단일 경우
-		if(dp[N] == null) {
-			dp[N] = Math.max(find(N - 2), find(N - 3) + score[N - 1]) + score[N];
+	static int find(int n) {
+		if (dp[n] == -1) {
+			dp[n] = Math.max(find(n - 2), find(n - 3) + arr[n - 1]) + arr[n];
 		}
-		
-		return dp[N];
+		return dp[n];
 	}
 }
