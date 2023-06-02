92654d2b32e65db3d6152347cf5346aa0be7854c
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-06-02 11:32:39 +0900

    [Gold III] Title: 연구소 3, Time: 260 ms, Memory: 40008 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -7,16 +7,15 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m, result = Integer.MAX_VALUE;
-	static int[][] arr;
-	static ArrayList<Node> virusList;
 
-	static Node[] activeVirusList;
-	static boolean[][] visited;
+	static int n, m, result = Integer.MAX_VALUE;
+	static int emptySpace = 0;
+	static int[][] map;
 	static int[] dx = { -1, 1, 0, 0 };
 	static int[] dy = { 0, 0, -1, 1 };
+	static ArrayList<Node> list;
+	static Node[] activeVirusList;
 
-	static int emptySpace=0;
 	static class Node {
 		int x, y;
 
@@ -26,121 +25,81 @@ public class Main {
 		}
 	}
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		arr = new int[n][n];
-		virusList = new ArrayList<>();
 
+		map = new int[n][n];
+		list = new ArrayList<>();
 		activeVirusList = new Node[m];
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
-				int input = Integer.parseInt(st.nextToken());
-				arr[i][j] -= input;
-				if (arr[i][j] == -2)
-					virusList.add(new Node(i, j));
-				else if(arr[i][j]==0)
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 2)
+					list.add(new Node(i, j));
+				else if (map[i][j] == 0)
 					emptySpace++;
 			}
 		}
-        if (emptySpace == 0) { // 빈 칸이 없는 경우
-            System.out.println(0);
-            return;
-        }
 
-		//simulation2();
-		simulation(0,0);
-		System.out.println(result==Integer.MAX_VALUE?-1:result);
+		if (emptySpace == 0) {
+			System.out.println(0);
+			return;
+		}
+		comb(0, 0);
+
+		System.out.println(result == Integer.MAX_VALUE ? -1 : result);
 	}
 
-	static void simulation(int depth,int start) {
-		if(depth==m) {
+	static void comb(int idx, int count) {
+		if (count == m) {
 			spread(emptySpace);
 			return;
 		}
-		for (int i = start; i < virusList.size(); i++) {
-			activeVirusList[depth]=virusList.get(i);
-			simulation(depth+1,i+1);
+
+		for (int i = idx; i < list.size(); i++) {
+			activeVirusList[count] = list.get(i);
+			comb(i + 1, count + 1);
 		}
 	}
-
 
 	static void spread(int emptySpace) {
 		Queue<Node> q = new LinkedList<>();
-		boolean[][] visit = new boolean[n][n];
-		for(Node node :activeVirusList) {
-			q.add(node);
-			visit[node.x][node.y]=true;
+		boolean[][] visited = new boolean[n][n];
+		for (int i = 0; i < m; i++) {
+			q.add(activeVirusList[i]);
+			visited[activeVirusList[i].x][activeVirusList[i].y] = true;
 		}
-		int time=0;
-		while(!q.isEmpty()) {
-			int size= q.size();
-			for(int i=0;i<size;i++) {
+		int time = 0;
+		while (!q.isEmpty()) {
+			int size = q.size();
+			for (int j = 0; j < size; j++) {
 				Node node = q.poll();
-				
-				for(int j=0;j<4;j++) {
-					int xx = node.x + dx[j];
-					int yy = node.y + dy[j];
-					if (xx < 0 || yy < 0 || xx >= n || yy >= n || visit[xx][yy]||arr[xx][yy]==-1)
+				for (int i = 0; i < 4; i++) {
+					int xx = node.x + dx[i];
+					int yy = node.y + dy[i];
+
+					if (xx < 0 || yy < 0 || xx >= n || yy >= n || visited[xx][yy] || map[xx][yy] == 1)
 						continue;
-					if(arr[xx][yy]==0) {
+
+					if (map[xx][yy] == 0) {
 						emptySpace--;
-					} 
-					
-					q.add(new Node(xx,yy));
-					visit[xx][yy]=true;
+					}
+
+					q.add(new Node(xx, yy));
+					visited[xx][yy] = true;
 				}
-				
 			}
-			
 			time++;
-			if(time>=result) return;
-			if(emptySpace==0) {
-				result=Math.min(time,result);
-			}
-
+			if (time >= result)
+				return;
+			if (emptySpace == 0)
+				result = time;
 		}
-		
-	}
-	
-	
-	static void simulation2() {
-		for (int i = 0; i < virusList.size(); i++) {
-			//tempArr = arr.clone();
 
-			visited = new boolean[n][n];
-			//spread(virusList.get(i), 0);
-			
-			result=Math.min(check(),result);
-		}
 	}
 
-	
-	static void spread2(Node node, int count) {
-		visited[node.x][node.y]=true;
-		//tempArr[node.x][node.y]=count;
-		for (int j = 0; j < 4; j++) {
-			int xx = node.x + dx[j];
-			int yy = node.y + dy[j];
-			if (xx < 0 || yy < 0 || xx >= n || yy >= n || visited[xx][yy])
-				continue;
-			if(arr[xx][yy]==0) {
-				spread2(new Node(xx,yy), count + 1);
-			}
-		}
-	}
-	
-	static int check() {
-		
-		int c=0;
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-			//	c=Math.max(c,tempArr[i][j]);
-			}
-		}
-		return c;
-	}
 }
