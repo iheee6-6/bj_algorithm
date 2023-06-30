cd876944189d38d9fdea484e074bbfd7bb87f642
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-06-30 11:46:29 +0900

    [level 3] Title: 입국심사, Time: 0.41 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        
+        long start=0;
+        long end=(long)times[times.length-1]*n;
+        while(start<=end){
+            long mid = (start+end)/2;
+            long count=0;
+            for(int i=0;i<times.length;i++){
+                count+=mid/times[i];
+            }
+           
+            if(count<n){
+                start=mid+1;
+            }else{
+                end=mid-1;
+                answer=mid;
+            }
+        }
+        return answer;
+    }
+}
