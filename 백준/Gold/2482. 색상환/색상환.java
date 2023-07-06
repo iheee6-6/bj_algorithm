4da983580a5a272f17ce7758b8411fdee215e7bd
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-07-07 01:06:48 +0900

    [Gold III] Title: 색상환, Time: 160 ms, Memory: 19280 KB -BaekjoonHub

---
+++ 백준/Gold/2482. 색상환/색상환.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final int MOD = 1000000003;
+
+    static int n, k;
+    static int dp[][];
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
+
+        dp = new int[n + 1][n + 1];
+
+        for(int i = 1; i <= n; i++){
+            dp[i][1] = i;
+            dp[i][0] = 1;
+        }
+
+        for(int i = 3; i <= n; i++){
+            for(int j = 2; j <= (i + 1) / 2; j++){
+                dp[i][j] = (dp[i - 1][j] + dp[i - 2][j - 1]) % MOD;
+            }
+        }
+
+        bw.write((dp[n - 3][k - 1] + dp[n - 1][k]) % MOD + "\n");
+        bw.close();
+        br.close();
+    }
+
+}
