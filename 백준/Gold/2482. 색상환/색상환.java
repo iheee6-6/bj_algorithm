faa20d626730dfbc6d27bc2abcfa1b385f57719c
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-07-07 11:08:09 +0900

    [Gold III] Title: 색상환, Time: 144 ms, Memory: 18164 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.java
+++ 백준/Gold/2482. 색상환/색상환.java
@@ -1,33 +1,26 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static final int MOD = 1000000003;
 
-    static int n, k;
-    static int dp[][];
-
-    public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        k = Integer.parseInt(br.readLine());
-
-        dp = new int[n + 1][n + 1];
-
-        for(int i = 1; i <= n; i++){
-            dp[i][1] = i;
-            dp[i][0] = 1;
-        }
-
-        for(int i = 3; i <= n; i++){
-            for(int j = 2; j <= (i + 1) / 2; j++){
-                dp[i][j] = (dp[i - 1][j] + dp[i - 2][j - 1]) % MOD;
-            }
-        }
-
-        bw.write((dp[n - 3][k - 1] + dp[n - 1][k]) % MOD + "\n");
-        bw.close();
-        br.close();
-    }
+	static final int MOD=1000000003;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		int K= Integer.parseInt(br.readLine());
+		int[][] dp = new int[N+1][N+1]; //N자리까지 N개 뽑는 경우의 수
+		for(int i=0;i<=N;i++) {
+			dp[i][1]=i; //1개 뽑는 경우의 수		
+		}
+		
+		for(int i=4;i<=N;i++) {
+			for(int j=2;j<=K;j++) { //[i][1]까지는 이미 위에서 세팅했기 때문에 2부터 시작,
+				dp[i][j]=(dp[i-1][j]+dp[i-2][j-1])%MOD;
+			}
+		}
+		
+		System.out.println(dp[N][K]%MOD); 
+	}
 
 }
