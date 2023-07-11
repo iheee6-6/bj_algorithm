4d61230d0b4ac1c4a71bb738dbb1b6440cc22950
Haeun ~ <58504019+iheee6-6@users.noreply.github.com>
2023-07-11 10:55:35 +0900

    [Gold IV] Title: 여행, Time: 572 ms, Memory: 50976 KB -BaekjoonHub

--- 백준/Gold/2157. 여행/여행.java
+++ 백준/Gold/2157. 여행/여행.java
@@ -1,78 +1,70 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Node{
-        int index;
-        int score;
 
-        public Node(int index, int score) {
-            this.index = index;
-            this.score = score;
-        }
-    }
-    
-    private static int dp[][];
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =new StringTokenizer(bf.readLine());
+	static int n, m, k;
+	static int result=0;
+	
+	static class Node{
+		int idx;
+		int point;
+		Node(int idx,int point){
+			this.idx=idx;
+			this.point=point;
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+		ArrayList<Node>[] map= new ArrayList[n+1];
+		for(int i=1;i<=n;i++) {
+			map[i]= new ArrayList<>();
+		}
+		
+		for (int i = 0; i < k; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			if(e<s) continue;
+			map[s].add(new Node(e,p));
+		}
 
-        dp = new int[M+1][N+1];
-        List<Node> boards[] = new List[N+1];
-        for(int i=0;i<=N;i++){
-            boards[i]=new ArrayList<>();
-        }
-
-        for(int i=0;i<K;i++){
-            st = new StringTokenizer(bf.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            if(a > b){
-                continue;
-            }
-            boards[a].add(new Node(b,c));
-        }
-
-        int result=0;
-
-        Queue<Integer> q = new LinkedList<>();
-        q.add(1);
-        int cnt=1;
-
-        while(!q.isEmpty() && cnt < M){
-            int size = q.size();
-            while(size-- > 0){
-                int nowIndex = q.poll();
-                for(Node nextNode : boards[nowIndex]){
-
-                    int nextIndex = nextNode.index;
-                    int nextScore = dp[cnt][nowIndex]+nextNode.score;
-
-                    if(dp[cnt+1][nextIndex] >= nextScore){
-                        continue;
-                    }
-
-                    dp[cnt+1][nextIndex] = nextScore;
-                    q.add(nextIndex);
-                }
-            }
-            cnt++;
-
-        }
-
-        for(int i=2;i<=M;i++){
-            result = Integer.max(result,dp[i][N]);
-        }
-
-        System.out.println(result);
-    }
+		int[][] dp = new int[m+1][n+1];
+		//go(1, 0,0);
+		Queue<Integer> q = new LinkedList<>();
+		q.add(1);
+		int count=1;
+		while(!q.isEmpty()&&count<m) {
+			int size = q.size();
+			for(int z=0;z<size;z++) {
+				int idx=q.poll();
+				for(Node node:map[idx]) {
+					if(dp[count][idx]+node.point<=dp[count+1][node.idx]) continue;
+					dp[count+1][node.idx]=dp[count][idx]+node.point;
+					q.add(node.idx);
+				}
+			}
+			
+			count++;
+		}
+		
+		
+		for(int i=2;i<=m;i++) {
+			result=Math.max(result, dp[i][n]);
+		}
+		
+		System.out.println(result);
+	}
+	
 }
